9da4212a32e69d1a3c60e56a3977365da090b409
eg-heiti-christian <christianflickinger94@gmail.com>
2024-09-17 20:14:56 -0400

    Creating base repository CRUD operations

--- src/database/base-repository.ts
+++ src/database/base-repository.ts
@@ -1,9 +1,29 @@
-
+import { isNumber } from 'lodash';
 export class BaseRepository<T> {
 
-    model: any;
+    private model: any;
     constructor(model: any) {
         this.model = model;
+    }
+
+    async getAll(skip?: number, take?: number) {
+        if (isNumber(skip) && isNumber(take)) {
+            return await this.model.findMany({
+                skip: skip,
+                take: take,
+            });
+        } else {
+            return await this.model.findMany();
+        }
+        
+    }
+
+    async getById(id: number) {
+        return await this.model.findUnique({
+            where: {
+                id: id
+            }
+        });
     }
 
     async create(object: T) {
@@ -12,5 +32,24 @@ export class BaseRepository<T> {
                 ...object
             },
         })
+    }
+
+    async update(id: number, object: T) {
+        return await this.model.update({
+            where: {
+                id: id,
+            },
+            data: {
+                ...object
+            }
+        });          
+    }
+
+    async delete(id: number) {
+        return await this.model.delete({
+            where: {
+                id: id
+            }
+        });
     }
 }
