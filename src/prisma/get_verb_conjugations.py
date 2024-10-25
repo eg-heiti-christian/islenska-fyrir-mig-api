1e9e4ceddf774b2ad5ceaaeccae6db81bd3b5793
eg-heiti-christian <christianflickinger94@gmail.com>
2024-10-24 21:38:15 -0400

    Update python script to read verbs from .csv
    - added .csv verbs seed file

--- src/prisma/get_verb_conjugations.py
+++ src/prisma/get_verb_conjugations.py
@@ -1,10 +1,16 @@
 import json
+import csv
+import os
 from islenska import Bin
 b = Bin()
 
-verbs = [
-    "eiga",
-]
+verbs_array = []
+
+with open('./verbs_seed.csv', newline='', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader:
+        verbs_array = row
+
 present_tense_inflection_tags = {
     "singularFirstPerson": "GM-FH-NT-1P-ET",
     "pluralFirstPerson": "GM-FH-NT-1P-FT",
@@ -23,17 +29,23 @@ past_tense_inflection_tags = {
 }
 
 verb_inflections = {}
-for verb in verbs:
+for verb in verbs_array:
     verb_inflections[verb] = {}
     verb_inflections[verb]["present_tense"] = {}
     verb_inflections[verb]["past_tense"] = {}
     for key in present_tense_inflection_tags:
         ksnid_list = b.lookup_variants(verb, "so", present_tense_inflection_tags[key])
-        verb_inflections[verb]["present_tense"][key] = ksnid_list[0].bmynd
+        if (len(ksnid_list) != 0):
+            verb_inflections[verb]["present_tense"][key] = ksnid_list[0].bmynd
+        else:
+            print("Could not find present ksnid for verb: " + verb)
 
     for key in past_tense_inflection_tags:
         ksnid_list = b.lookup_variants(verb, "so", past_tense_inflection_tags[key]) 
-        verb_inflections[verb]["past_tense"][key] = ksnid_list[0].bmynd
+        if (len(ksnid_list) != 0):
+            verb_inflections[verb]["past_tense"][key] = ksnid_list[0].bmynd
+        else:
+            print("Could not find past ksnid for verb: " + verb)
 
 with open('verb_inflections.ts', 'w') as fp:
     json.dump(verb_inflections, fp)
